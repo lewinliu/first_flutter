1ac9ca2ee1dd933556a99615b12593636f974240
charles <charles@ying31.com>
2019-06-17 10:07:32 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+///入口函数
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Hello Flutter !'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var a = "123";
+    String b = "123";
+    dynamic c = 1;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              '你点击了这个按钮多少次:${a == b},${c = a + b}',
+            ),
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.display1,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
---
+++ lib/test.dart
@@ -0,0 +1,23 @@
+//单行注释
+///文档注释
+/**
+ * 多行注释
+ */
+
+var content = 'Dart 语法'; // 声明并初始化变量
+String _name = "小明"; //以下划线 _ 开头的类或成员变量是私有的
+int a = 1;
+double b = 0.1;
+
+String str = "$_name:a+b=${a + b}";
+
+dynamic example = 'example';
+
+bool isEmpty(String str) {
+  return str.isEmpty;
+}
+
+class A{
+
+  
+}
